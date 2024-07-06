8fb81e95d275832df0f9821c68e4c053ed0fd72a
dvinutha2861999 <d.vinuta28@gmail.com>
2024-07-06 23:30:26 +0530

    commit

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The gas usually filled in the electric bulb is",
+    answer: "Nitrogen",
+    options: [
+      "Nitrogen",
+      "Hydrogen",
+      "Carbon dioxide",
+      "Oxygen"
+    
+    ]
+  },
+    {
+    numb: 2,
+    question: "Washing soda is the common name for",
+    answer: "Sodium carbonate",
+    options: [
+      "Calcium bicarbonate",
+      "Sodium carbonate",
+      "Sodium bicarbonate",
+      "Calcium carbonate"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quartz crystals normally used in quartz clocks etc. is chemically",
+    answer: "Silicon dioxide",
+    options: [
+      "Sodium silicate",
+      "Germanium oxide",
+      "A mixture of germanium oxide and silicon dioxide",
+      "Silicon dioxide"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The members of Lok Sabha hold office for a term of",
+    answer: " 5 years",
+    options: [
+      "4 years",
+      " 5 years",
+      "6 years",
+      "7 years"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+    numb: 6,
+     question: "The Radcliffe line is a boundary between",
+     answer: "India and Pakistan",
+     options: [
+       "India and China",
+       "India and Pakistan",
+       "India and Myanmar",
+       "India and Afghanistan"
+     ]
+   },
+];
